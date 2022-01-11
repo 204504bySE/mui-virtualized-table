08115928fe5fea0ee4258bff168d8a0dd622cd1e
204504bySE <204504byse@204504byse.info>
2022-01-12 03:28:26 +0900

    Migrate storybook configuration following "From version 5.3.x to 6.0.x" -> "6.0 Deprecations"

--- stories/preview.js
+++ stories/preview.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import { addDecorator } from '@storybook/react';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 
 const theme = createTheme();
 
-addDecorator(storyFn => (
-  <React.Fragment>
-    <CssBaseline />
-    <ThemeProvider theme={theme}>
-      {storyFn()}
-    </ThemeProvider>
-  </React.Fragment>
-));
+export const decorators = [
+  (storyFn) => (
+    <React.Fragment>
+      <CssBaseline />
+      <ThemeProvider theme={theme}>
+        {storyFn()}
+      </ThemeProvider>
+    </React.Fragment>
+  )
+];
