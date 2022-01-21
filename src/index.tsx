feca89573aee491b9c0fc33da61f0dd184c5b252
204504bySE <204504byse@204504byse.info>
2022-01-21 19:56:50 +0900

    make useStyles replacable with prop

--- src/index.tsx
+++ src/index.tsx
@@ -8,125 +8,12 @@ import TableFooter from '@mui/material/TableFooter';
 import TablePagination, { TablePaginationProps } from '@mui/material/TablePagination';
 import TableSortLabel from '@mui/material/TableSortLabel';
 import { Theme, useTheme } from '@mui/material/styles';
-import { makeStyles } from '@mui/styles';
+
 import Draggable from 'react-draggable';
 import { calcColumnWidth, Column } from './utils';
+import { FOOTER_BORDER_HEIGHT, useDefaultStyles } from './style';
 
-const FOOTER_BORDER_HEIGHT = 1;
 
-const useStyles = makeStyles((theme: Theme) => ({
-  table: {
-    boxSizing: 'border-box',
-
-    '& .topLeftGrid': {
-      backgroundColor:
-        theme.palette.grey[theme.palette.mode === 'dark' ? 800 : 200],
-      borderBottom: `2px solid ${theme.palette.divider}`,
-      borderRight: `2px solid ${theme.palette.divider}`,
-      color: theme.palette.text.secondary,
-      fontSize: theme.typography.pxToRem(12),
-
-      // Hide scrollbars
-      '&::-webkit-scrollbar': { //Chrome/Safari
-        display: 'none'
-      },
-      'scrollbar-width': 'none',  //Firefox
-      '-ms-overflow-style': 'none'  //IE
-    },
-
-    '& .topRightGrid': {
-      backgroundColor:
-        theme.palette.grey[theme.palette.mode === 'dark' ? 800 : 200],
-      borderBottom: `2px solid ${theme.palette.divider}`,
-      color: theme.palette.text.secondary,
-      fontSize: theme.typography.pxToRem(12),
-
-      // Hide scrollbars
-      '&::-webkit-scrollbar': { //Chrome/Safari
-        display: 'none'
-      },
-      'scrollbar-width': 'none',  //Firefox
-      '-ms-overflow-style': 'none'  //IE
-    },
-
-    '& .bottomLeftGrid': {
-      backgroundColor:
-        theme.palette.grey[theme.palette.mode === 'dark' ? 800 : 200],
-      borderRight: `2px solid ${theme.palette.divider}`,
-      color: theme.palette.text.secondary,
-      fontSize: theme.typography.pxToRem(13),
-
-      // Hide scrollbars
-      '&::-webkit-scrollbar': { //Chrome/Safari
-        display: 'none'
-      },
-      'scrollbar-width': 'none',  //Firefox
-      '-ms-overflow-style': 'none'  //IE
-    },
-
-    '& .bottomRightGrid': {
-      color: theme.palette.text.primary,
-      fontSize: theme.typography.pxToRem(13),
-      outline: 'none' // See: https://github.com/bvaughn/react-virtualized/issues/381
-    }
-  },
-  cell: {
-    boxSizing: 'border-box',
-    display: 'flex',
-    alignItems: 'center'
-  },
-  cellClickable: {
-    cursor: 'pointer'
-  },
-  cellSelected: {
-    backgroundColor:
-      theme.palette.grey[theme.palette.mode === 'dark' ? 900 : 100]
-  },
-  cellHovered: {
-    backgroundColor:
-      theme.palette.grey[theme.palette.mode === 'dark' ? 800 : 200]
-  },
-  cellDisabled: {
-    opacity: 0.5
-  },
-  cellContents: {
-    width: '100%',
-    whiteSpace: 'nowrap',
-    overflow: 'hidden',
-    textOverflow: 'ellipsis'
-  },
-  cellHeader: {
-    fontSize: theme.typography.pxToRem(12),
-    fontWeight: theme.typography.fontWeightMedium,
-    color: theme.palette.text.secondary
-  },
-  cellInLastColumn: {
-    paddingRight: theme.spacing(3)
-  },
-  cellInLastRow: {
-    borderBottom: 'none'
-  },
-  footer: {
-    borderTop: `${FOOTER_BORDER_HEIGHT}px solid ${theme.palette.divider}`
-  },
-  dragHandle: {
-    flex: '0 0 16px',
-    zIndex: 2,
-    cursor: 'col-resize',
-    color: '#0085ff'
-  },
-  DragHandleActive: {
-    color: '#0b6fcc',
-    zIndex: 3
-  },
-  DragHandleIcon: {
-    flex: '0 0 12px',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
-}));
 
 const calculateWidths = ({ resizable, columns: Columns }: {resizable: Boolean, columns: any[]}) => {
   var widths: {[key: string]: number} = {};
@@ -402,6 +289,7 @@ export default function MuiVirtualizedTable({
   fixedColumnCount = 0,
   rowHeight = 48,
   style,
+  useStyles,
   columnWidth,
   includeHeaders = false,
   isCellHovered,
@@ -420,7 +308,7 @@ export default function MuiVirtualizedTable({
 }: MultiGridProps & CellRendererType & {
   pagination: TablePaginationProps,  
 }) {
-  const classes = useStyles({ classes: Classes });
+  const classes = (useStyles ?? useDefaultStyles)({ classes: Classes });
   const theme = useTheme();
 
   const multiGrid = React.useRef<MultiGrid>(null);
@@ -459,9 +347,6 @@ export default function MuiVirtualizedTable({
       : calculatedHeightWithFooter;
   const multiGridHeight = containerHeight - (pagination ? paginationHeight : 0);
 
-  const columnCount = Array.isArray(columns) ? columns.length : 0;
-  const rowCount = Array.isArray(data) ? data.length + (includeHeaders ? 1 : 0) : 0;
-
   return (
     <Table
       component='div'
@@ -497,7 +382,7 @@ export default function MuiVirtualizedTable({
         enableFixedColumnScroll={fixedColumnCount > 0}
         height={multiGridHeight}
         rowHeight={rowHeight}
-        rowCount={rowCount}
+        rowCount={Array.isArray(data) ? data.length + (includeHeaders ? 1 : 0) : 0}
         fixedRowCount={fixedRowCount}
         enableFixedRowScroll={fixedRowCount > 0}
         // TODO: Read these from `classes` without classes.table inheritance?  How to pass props.classes down to override?
@@ -540,6 +425,7 @@ MuiVirtualizedTable.propTypes = {
   isCellDisabled: PropTypes.func,
   classes: PropTypes.object,
   cellProps: PropTypes.oneOfType([PropTypes.object, PropTypes.func]),
-  style: PropTypes.object
+  style: PropTypes.object,
+  useStyles: PropTypes.func,
 };
 
---
+++ src/style.ts
@@ -0,0 +1,117 @@
+import { makeStyles } from '@mui/styles';
+import { Theme } from '@mui/material/styles';
+
+export const FOOTER_BORDER_HEIGHT = 1;
+export const useDefaultStyles = makeStyles((theme: Theme) => ({
+  table: {
+    boxSizing: 'border-box',
+
+    '& .topLeftGrid': {
+      backgroundColor:
+        theme.palette.grey[theme.palette.mode === 'dark' ? 800 : 200],
+      borderBottom: `2px solid ${theme.palette.divider}`,
+      borderRight: `2px solid ${theme.palette.divider}`,
+      color: theme.palette.text.secondary,
+      fontSize: theme.typography.pxToRem(12),
+
+      // Hide scrollbars
+      '&::-webkit-scrollbar': { //Chrome/Safari
+        display: 'none'
+      },
+      'scrollbar-width': 'none',  //Firefox
+      '-ms-overflow-style': 'none'  //IE
+    },
+
+    '& .topRightGrid': {
+      backgroundColor:
+        theme.palette.grey[theme.palette.mode === 'dark' ? 800 : 200],
+      borderBottom: `2px solid ${theme.palette.divider}`,
+      color: theme.palette.text.secondary,
+      fontSize: theme.typography.pxToRem(12),
+
+      // Hide scrollbars
+      '&::-webkit-scrollbar': { //Chrome/Safari
+        display: 'none'
+      },
+      'scrollbar-width': 'none',  //Firefox
+      '-ms-overflow-style': 'none'  //IE
+    },
+
+    '& .bottomLeftGrid': {
+      backgroundColor:
+        theme.palette.grey[theme.palette.mode === 'dark' ? 800 : 200],
+      borderRight: `2px solid ${theme.palette.divider}`,
+      color: theme.palette.text.secondary,
+      fontSize: theme.typography.pxToRem(13),
+
+      // Hide scrollbars
+      '&::-webkit-scrollbar': { //Chrome/Safari
+        display: 'none'
+      },
+      'scrollbar-width': 'none',  //Firefox
+      '-ms-overflow-style': 'none'  //IE
+    },
+
+    '& .bottomRightGrid': {
+      color: theme.palette.text.primary,
+      fontSize: theme.typography.pxToRem(13),
+      outline: 'none' // See: https://github.com/bvaughn/react-virtualized/issues/381
+    }
+  },
+  cell: {
+    boxSizing: 'border-box',
+    display: 'flex',
+    alignItems: 'center'
+  },
+  cellClickable: {
+    cursor: 'pointer'
+  },
+  cellSelected: {
+    backgroundColor:
+      theme.palette.grey[theme.palette.mode === 'dark' ? 900 : 100]
+  },
+  cellHovered: {
+    backgroundColor:
+      theme.palette.grey[theme.palette.mode === 'dark' ? 800 : 200]
+  },
+  cellDisabled: {
+    opacity: 0.5
+  },
+  cellContents: {
+    width: '100%',
+    whiteSpace: 'nowrap',
+    overflow: 'hidden',
+    textOverflow: 'ellipsis'
+  },
+  cellHeader: {
+    fontSize: theme.typography.pxToRem(12),
+    fontWeight: theme.typography.fontWeightMedium,
+    color: theme.palette.text.secondary
+  },
+  cellInLastColumn: {
+    paddingRight: theme.spacing(3)
+  },
+  cellInLastRow: {
+    borderBottom: 'none'
+  },
+  footer: {
+    borderTop: `${FOOTER_BORDER_HEIGHT}px solid ${theme.palette.divider}`
+  },
+  dragHandle: {
+    flex: '0 0 16px',
+    zIndex: 2,
+    cursor: 'col-resize',
+    color: '#0085ff'
+  },
+  DragHandleActive: {
+    color: '#0b6fcc',
+    zIndex: 3
+  },
+  DragHandleIcon: {
+    flex: '0 0 12px',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+}));
